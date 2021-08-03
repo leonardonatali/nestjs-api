e41a008b1631c0e9afba82460235852ff6a9f8fa
Leonardo Natali <leonardo.natali@hotmail.com>
2021-08-02 23:49:56 -0300

    feat: mock response for players controller

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common'
+import { CreatePlayerDto } from './entities/dtos/create.player.dto'
 
 @Controller('players')
-export class PlayersController {}
+export class PlayersController {
+    @Post()
+  async savePlayer (@Body() createPlayer: CreatePlayerDto) {
+    return createPlayer
+  }
+}
