2955c62a6f6759865a6ef7b58af44cb62b824daf
Leonardo Natali <leonardo.natali@hotmail.com>
2021-08-04 22:25:09 -0300

    feat: add in memory storage for players

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,10 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, Get, Post } from '@nestjs/common'
 import { CreatePlayerDto } from './entities/dtos/create.player.dto'
+import { PlayersService } from './players.service'
 
 @Controller('players')
 export class PlayersController {
+  private playersService: PlayersService
+
+  constructor (playersService: PlayersService) {
+    this.playersService = playersService
+  }
+
     @Post()
   async savePlayer (@Body() createPlayer: CreatePlayerDto) {
+    await this.playersService.save(createPlayer)
     return createPlayer
   }
+
+  @Get()
+    async index () {
+      return this.playersService.getAll()
+    }
 }
--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger } from '@nestjs/common'
+import { randomUUID } from 'crypto'
+import { CreatePlayerDto } from './entities/dtos/create.player.dto'
+import { Player } from './entities/player.entity'
 
 @Injectable()
-export class PlayersService {}
+export class PlayersService {
+    private players: Player[] = []
+    private readonly logger = new Logger(PlayersService.name)
+
+    async save (savePlayerDto: CreatePlayerDto) {
+      this.logger.log('new player has been created')
+      this.create(savePlayerDto)
+    }
+
+    async getAll (): Promise<Player[]> {
+      return this.players
+    }
+
+    private async create (createPlayerDto: CreatePlayerDto): Promise<void> {
+      const player: Player = {
+        _id: randomUUID(),
+        email: createPlayerDto.email,
+        name: createPlayerDto.name,
+        phone: createPlayerDto.phone,
+        generalPosition: 1,
+        positionRanking: 1,
+        ranking: 'A'
+      }
+
+      this.players.push(player)
+    }
+}
